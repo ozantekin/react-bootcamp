b1a6fdb47f36ddac2b392cb40cdf5724b30dad8c
Ozan Tekin <ardilozantekin@gmail.com>
2022-05-19 01:30:10 +0300

    Update Pokedex.js

--- 04_pokedex_project/src/Pokedex.js
+++ 04_pokedex_project/src/Pokedex.js
@@ -3,18 +3,6 @@ import Pokecard from './Pokecard'
 import './Pokedex.css'
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-      { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-      { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-      { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-      { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-      { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-      { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-      { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
-    ],
-  }
   render() {
     let title
     if (this.props.isWinner) {
