86356ed171528402d2d85c43d4a8067702f31b45
Ozan Tekin <ardilozantekin@gmail.com>
2022-05-25 12:43:03 +0300

    mutating state tamamlandı

---
+++ 07_State_Patterns/01_mutating_state/src/App.js
@@ -0,0 +1,12 @@
+import IconList from './IconList.js'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <IconList />
+    </div>
+  )
+}
+
+export default App
---
+++ 07_State_Patterns/01_mutating_state/src/IconList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class IconList extends Component {
+  static defaultProps = {
+    options: [
+      'angry',
+      'dizzy',
+      'flushed',
+      'frown',
+      'grimace',
+      'grin',
+      'kiss',
+      'meh',
+      'tired',
+      'surprise',
+    ],
+  }
+  constructor(props) {
+    super(props)
+    this.state = { icons: [] }
+    this.addIcon = this.addIcon.bind(this)
+  }
+
+  // addIcon() {
+  //   let idx = Math.floor(Math.random() * this.props.options.length)
+  //   let newIcon = this.props.options[idx]
+  //   let icons = this.state.icons
+  //   icons.push(newIcon)
+  //   this.setState({ icons: icons })
+  // }
+
+  // diğer yöntem
+  addIcon() {
+    let idx = Math.floor(Math.random() * this.props.options.length)
+    let newIcon = this.props.options[idx]
+    this.setState({ icons: [...this.state.icons, newIcon] })
+  }
+
+  render() {
+    const icons = this.state.icons.map((i) => (
+      <i className={`fa-solid fa-face-${i}`}></i>
+    ))
+    return (
+      <div>
+        <h1>Günün Emojisi: {icons} </h1>
+        <button onClick={this.addIcon}>Modunu görmek için tıkla</button>
+      </div>
+    )
+  }
+}
+
+export default IconList
