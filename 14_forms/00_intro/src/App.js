70370ba33798ffefc71b05fad34db2f7f6ace7d8
Ozan Tekin <ardilozantekin@gmail.com>
2022-06-07 20:04:50 +0300

    introduction to form demo

---
+++ 14_forms/00_intro/src/App.js
@@ -0,0 +1,12 @@
+import Form from './Form.js'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ 14_forms/00_intro/src/Form.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { userName: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    alert(`Adınızın ${this.state.userName} olduğundan emin misiniz?`)
+    this.setState({
+      userName: '',
+    })
+  }
+  handleChange(e) {
+    this.setState({
+      userName: e.target.value,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Introduction to Form Demo</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label for='fullname'>Adınız: </label>
+          <input
+            type='text'
+            name='fullname'
+            value={this.state.userName}
+            onChange={this.handleChange}
+          ></input>
+          <button>Devam Et</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default Form
